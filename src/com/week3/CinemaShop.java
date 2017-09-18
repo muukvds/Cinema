e8bcb4edf520247bdf7f82ae2f0e89e715198094
muukvds <muukvds@users.noreply.github.com>
2017-09-18 22:49:28 +0200

    add Snack and Drink Supply array and modifier to CinemaShop class

--- src/com/week3/CinemaShop.java
+++ src/com/week3/CinemaShop.java
@@ -2,4 +2,30 @@ package com.week3;
 
 public class CinemaShop {
 
+    private DrinkSupply[] drinkSupplies = new DrinkSupply[15];
+    private SnackSupply[] snackSupplies = new SnackSupply[10];
+
+    public void addDrinkSupplie(DrinkSupply drinkSupplie) {
+        for(int i =0;1<drinkSupplies.length;i++)
+        {
+            if(drinkSupplies[i] != null)
+            {
+                drinkSupplies[i]=drinkSupplie;
+                break;
+            }
+        }
+    }
+
+    public void addSnackSupplie(SnackSupply snackSupplie) {
+
+        for(int i =0;1<snackSupplies.length;i++)
+        {
+            if(snackSupplies[i] != null)
+            {
+                snackSupplies[i]=snackSupplie;
+                break;
+            }
+        }
+
+    }
 }
