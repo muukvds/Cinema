b604b8f106e1ce3198bea69e3f1a093b97554f17
muukvds <muukvds@users.noreply.github.com>
2017-09-18 22:40:42 +0200

    add getters and setters for Cinema class

--- src/com/week3/Cinema.java
+++ src/com/week3/Cinema.java
@@ -5,4 +5,19 @@ public class Cinema {
     private String name;
     private CinemaShop cinemaShop;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CinemaShop getCinemaShop() {
+        return cinemaShop;
+    }
+
+    public void setCinemaShop(CinemaShop cinemaShop) {
+        this.cinemaShop = cinemaShop;
+    }
 }
