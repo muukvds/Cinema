55e2c980e123dcd0af28063036e7ef03044ace98
muukvds <muukvds@users.noreply.github.com>
2017-09-18 22:42:23 +0200

    add getters and setters to Snack and Drink Supply classes

--- src/com/week3/DrinkSupply.java
+++ src/com/week3/DrinkSupply.java
@@ -4,4 +4,16 @@ public class DrinkSupply {
     private String name;
     private int price;
     private int supply;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setSupply(int supply) {
+        this.supply = supply;
+    }
 }
